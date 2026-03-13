5d1f5104cc0c2b1b2a0068aa1be021ddebb9d816
gurdian0614 <ivan.gurdian@outlook.com>
2026-03-12 20:07:36 -0600

    Create GestorUSuarios.tsx

---
+++ src/components/GestorUSuarios.tsx
@@ -0,0 +1,44 @@
+import UserForm from "./UserForm"; 
+import UserList from "./UserList";
+import useUser from "../hooks/useUser";
+
+const GestorUsuarios = () => {
+    const {
+        users,
+        userToEdit,
+        setUserToEdit,
+        loading,
+        formData,
+        setFormData,
+        handleInputChange,
+        handleSubmit,
+        handleDelete,
+        handleEdit,
+    } = useUser();
+    return (
+        <div className="container mx-auto max-w-7xl">
+            <h1 className="text-4xl md:text-5xl font-bold text-center text-gray-800 mb-10 tracking-tight">
+                Gestión de Usuarios
+            </h1>
+
+            <UserForm
+                formData={formData}
+                setFormData={setFormData}
+                handleInputChange={handleInputChange}
+                handleSubmit={handleSubmit}
+                userToEdit={userToEdit}
+                setUserToEdit={setUserToEdit}
+                loading={loading}
+            />
+
+            <UserList
+                users={users}
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+                loading={loading} 
+            />
+        </div>
+    );
+}
+
+export default GestorUsuarios;
